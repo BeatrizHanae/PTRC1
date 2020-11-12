264a25d29d3952b3d0b06f38f743f9b362c41670
AlineTeles <alinetelesfranca@gmail.com>
2020-11-12 19:59:27 -0300

    atualizando pág de suporte, e crianda a pág login/funcionario

--- src/pages/LoginFuncionario/index.jsx
+++ src/pages/LoginFuncionario/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
-
+import { Container } from '../FuncionarioCadastro/styles';
+import Button from '../../components/Button';
+import cliente1 from '../../assets/cliente1.png';
+import image1 from '../../assets/image1.png';
+import orcamento1 from '../../assets/orcamento1.png';
+import { DivBotao, DivTexto } from './styles';
 const LoginFuncionario = () =>{
+ return(
+   <Container>
+      <DivTexto>
+        <h2>
+          Título
+        </h2>
+        <p>
+            Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, 
+           als ein unbekannter Schriftsteller eine Hand voll Wörter nahm und diese durcheinander warf um ustrie.
+           Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, als ein un
+        </p>
+       
+      </DivTexto>  
+      <DivBotao>
+          <Button>
+            <img src={image1} alt= "cadastrar produto" />
+            Cadastrar Produto
+          </Button>
+          <Button>
+            <img src={orcamento1} alt=" orçamento" />
+            Orçamento e negociações
+          </Button>
+          <Button>
+           <img src= {cliente1} alt="cliente" />
+           Clientes
+          </Button>     
+      </DivBotao>
+   </Container>
  
+ )
 
 }
 
--- src/pages/Suporte/index.jsx
+++ src/pages/Suporte/index.jsx
@@ -3,43 +3,60 @@ import React from 'react';
 import IconAssistenciaTecnica from '../../assets/IconAssistenciaTecnica.png';
 import IconManutencao from '../../assets/IconManutencao.png';
 import IconTreinamento from '../../assets/IconTreinamento.png';
-import { Container, DivAssistencia, DivManutencao } from './styles';
+import { Container, DivAgrupa, DivAssistencia, DivBotao, DivManutencao } from './styles';
+import Button from '../../components/Button';
 
 const Suporte = () => {
   return (
     <Container>
-      <DivManutencao>
-        <img src={IconManutencao} alt ="Icone da manutenção"/>
-        <h1>Manutenção</h1>
-        <p>Para realizar a manutenção dos
-        produtos baixe os nossos manuais:
-        </p>
-        <p>
-          -> Entre em contato contato@nihon.com
-        </p>
-        <p>
-          -> Ou clique aqui!
-        </p>
-        <img src={IconTreinamento} alt ="Icone de treinamento"/>
-        <h1>Treinamento</h1>
-        <p>
-          -> Ou clique aqui!
-        </p>
-      </DivManutencao>
+      <DivBotao>
+        <h1>
+          Suporte
+        </h1>
+        <h2>
+          Dúvidas frenquetes
+        </h2>
+        <Button>Como posso comprar algum produto? </Button>
+        <Button>Quais são as formar de pagamento? </Button>
+        <Button>Qual é o prazo para entrega do produto? </Button>
+        <Button>O que fazer se o produto estiver com um defeito? </Button>
+      </DivBotao>
 
-      <DivAssistencia>
-      <img src={IconAssistenciaTecnica} alt ="Icone da Assistencia"/>
-        <h1>Assistência técnica </h1>
-        <p>
-          Caso precise de assistência técnica,
-        </p>
-        <p>
-          entre em contato para resolver seu problema:
-        </p>
-        <p>
-         (61) 3877-2496
-        </p>
-      </DivAssistencia>
+      <DivAgrupa>
+        <DivManutencao>
+          <img src={IconManutencao} alt ="Icone da manutenção"/>
+          <h1>Manutenção</h1>
+          <p>Para realizar a manutenção dos
+          produtos baixe os nossos manuais:
+          </p>
+          <p>
+            -> Entre em contato contato@nihon.com
+          </p>
+          <p>
+            -> Ou clique aqui!
+          </p>
+          <img src={IconTreinamento} alt ="Icone de treinamento"/>
+          <h1>Treinamento</h1>
+          <p>
+            -> Ou clique aqui!
+          </p>
+        </DivManutencao>
+
+        <DivAssistencia>
+        <img src={IconAssistenciaTecnica} alt ="Icone da Assistencia"/>
+          <h1>Assistência técnica </h1>
+          <p>
+            Caso precise de assistência técnica,
+          </p>
+          <p>
+            entre em contato para resolver seu problema:
+          </p>
+          <p>
+          (61) 3877-2496
+          </p>
+        </DivAssistencia>
+      </DivAgrupa>
+      <h1>A jornada inteligente na automação comercial!</h1>
       
     </Container>
 
